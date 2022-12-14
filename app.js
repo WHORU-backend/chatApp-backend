b0fcc93e6504a7e6a995a44f0632bda69ba27fbc
Kim minji <enddl3224@gmail.com>
2022-12-14 15:50:05 +0900

    inital-settings

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+require("express-async-errors");
+const router = require("./routes");
+const morgan = require("morgan");
+const cors = require("cors");
+const myDataSource = require("./models/db.config");
+
+// DB 연결
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("DB Connection");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const createApp = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(cors());
+  app.use(router);
+
+  return app;
+};
+
+module.exports = { createApp };
