d9d85ba1f97832cf97def5567e81c85c4f2f3771
Kim minji <enddl3224@gmail.com>
2022-12-18 17:49:10 +0900

    Add: 이메일 회원가입

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const {userService} = require('../services');
+
+const signUp = async (req, res) => {
+  const userDto = {...req.body};
+  
+  if (!(userDto.name && userDto.email && userDto.password)) {
+    res.status(400).json({ error: "KEY ERROR" });
+    return;
+  }
+
+  try {
+    await userService.signUp(userDto);
+    res.status(201).json({ message: "SUCCESS" });
+  } catch (err) {
+    console.log(err);
+    res.status(err.statusCode || 500).json({ error: err.message });
+  }
+};
+
+module.exports = { 
+    signUp,
+}
