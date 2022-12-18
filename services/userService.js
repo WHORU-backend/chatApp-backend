fa87044c2406d1043c0ee2a6a02fa8de9eec4405
Kim minji <enddl3224@gmail.com>
2022-12-18 17:49:10 +0900

    Add: 이메일 회원가입

---
+++ services/userService.js
@@ -0,0 +1,11 @@
+const {userDao} = require('../models');
+const bcrypt = require("bcrypt");
+
+const signUp = async(userDto)=>{
+    userDto.password =  await bcrypt.hash(userDto.password, 12);
+    await userDao.createUser(userDto);
+}
+
+module.exports = { 
+    signUp,
+}
