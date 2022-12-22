0a96fe0e95cc6a5c2cbe1e0286226cef93dd0965
Kim minji <enddl3224@gmail.com>
2022-12-22 18:28:55 +0900

    Update: 정규식 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,6 +8,16 @@ const signUp = async (req, res) => {
     return;
   }
 
+  var emailForm = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;;
+  const passwordForm = new RegExp("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})");
+
+  if(!emailForm.test(userDto.email)){
+    return res.status(400).json({message: 'INVALID EMAIL'})
+  }
+  if(!passwordForm.test(userDto.password)){
+    return res.status(400).json({message: 'INVALID PASSWORD'})
+  }
+
   try {
     await userService.signUp(userDto);
     res.status(201).json({ message: "SUCCESS" });
@@ -19,4 +29,4 @@ const signUp = async (req, res) => {
 
 module.exports = { 
     signUp,
-}
+};
